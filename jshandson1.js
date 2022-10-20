b11ad23e20084ad888211bf73c2192ad89a3e45f
TAMILARASAN S <111070542+tamil6268@users.noreply.github.com>
2022-10-20 11:59:13 +0530

    JS-Handson 1

---
+++ jshandson1.js
@@ -0,0 +1,11 @@
+const AddTwoNumbers = (a,b) => { 
+             return a+b;
+            }
+const Is_Valid = (a,b) => {
+        if(a<10 && a>b){
+         return true
+        }
+        else{
+          return false
+        }
+      };   
